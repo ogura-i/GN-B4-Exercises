cd28d2d289549bf3ea383d7a9c325f4beb1fd439
Iori Ogura <ogura-i@swlab.cs.okayama-u.ac.jp>
2018-03-16 17:00:55 +0900

    Edit Myslackbot.rb and test.json

--- Exercise-2017/SlackBot/MySlackBot.rb
+++ Exercise-2017/SlackBot/MySlackBot.rb
@@ -1,10 +1,16 @@
+# coding: utf-8
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
 require 'sinatra'
 require 'SlackBot'
 
 class MySlackBot < SlackBot
-  # cool code goes here
+  def test(params, options={})
+    user_name = params[:user_name] ? "@#{params[:user_name]}" : ""
+    msg = params[:text]
+    msg = msg.match(/^「(.*)」と言って/)
+    return {text: "#{user_name} #{msg[1]}"}.merge(options).to_json
+  end
 end
 
 slackbot = MySlackBot.new
@@ -17,5 +23,6 @@ end
 
 post '/slack' do
   content_type :json
-  slackbot.naive_respond(params, username: "Bot")
+  slackbot.test(params, username: "Bot")
+  #  slackbot.naive_respond(params, username: "Bot")
 end
